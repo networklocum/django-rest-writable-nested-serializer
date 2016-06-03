4ab2a5fe4ed24e1b940afce21ab07cc7153aeaef
Michael Skelly <skellystudios@gmail.com>
2016-06-03 12:23:08 +0100

    Better name

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='django-rest-writable-nested-serializer',
       version='0.0.1',
       description='django-rest-writable-nested-serializer',
       url='https://github.com/networklocum/django-rest-writable-nested-serializer',
-      packages=['writablenestedserializers'],
+      packages=['writable_nested_serializers'],
       install_requires=["django>=1.6", "djangorestframework"])
